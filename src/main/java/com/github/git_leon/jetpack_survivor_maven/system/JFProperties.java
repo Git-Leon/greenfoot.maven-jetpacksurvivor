7ea04adc233059bc350f5e4936f533b299f8d692
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2018-07-29 11:07:09 -0400

    fixed compile issue

--- src/main/java/com/github/git_leon/jetpack_survivor_maven/system/JFProperties.java
+++ src/main/java/com/github/git_leon/jetpack_survivor_maven/system/JFProperties.java
@@ -1,17 +1,20 @@
 package com.github.git_leon.jetpack_survivor_maven.system;
 
 import com.github.git_leon.Introspection;
+import com.github.git_leon.jetpack_survivor_maven.utils.exceptions.JFootError;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
 public class JFProperties {
-    private final JFProperty PROJECT_NAME = new JFProperty("project.name");
-    private final JFProperty MAIN_CLASS = new JFProperty("main.class");
-    private final JFProperty SCENARIO_FILEINPUT= new JFProperty("scenario.fileInput");
-    private final JFProperty SCENARIO_HIDECONTROLS = new JFProperty("scenario.hideControls");
-    private final JFProperty SCENARIO_LOCK = new JFProperty("scenario.lock");
+    public final JFProperty PROJECT_NAME = new JFProperty("project.name");
+    public final JFProperty MAIN_CLASS = new JFProperty("main.class");
+    public final JFProperty SCENARIO_FILEINPUT= new JFProperty("scenario.fileInput");
+    public final JFProperty SCENARIO_HIDECONTROLS = new JFProperty("scenario.hideControls");
+    public final JFProperty SCENARIO_LOCK = new JFProperty("scenario.lock");
 
     public JFProperties(String... args) {
         args = args != null ? Arrays.copyOf(args, 5) : new String[5];
@@ -27,5 +30,13 @@ public class JFProperties {
         List<JFProperty> propertyList = Introspection.getFieldValues(this, JFProperty.class);
         propertyList.forEach(jfProperty -> properties.putAll(jfProperty.getProperty()));
         return properties;
+    }
+
+    public void load(InputStream is) {
+        try {
+            getProperties().load(is);
+        } catch (IOException e) {
+            throw new JFootError(e);
+        }
     }
 }
