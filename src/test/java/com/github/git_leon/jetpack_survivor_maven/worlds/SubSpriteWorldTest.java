a7bac76746dc15e443acd7f77ee1074365fb3beb
Leon Hunter <leonhunter@Leons-MacBook-Pro-2.local>
2018-08-13 14:33:10 -0400

    removed all deprecated classes

--- src/test/java/com/github/git_leon/jetpack_survivor_maven/worlds/SubSpriteWorldTest.java
+++ src/test/java/com/github/git_leon/jetpack_survivor_maven/worlds/SubSpriteWorldTest.java
@@ -1,6 +1,6 @@
 package com.github.git_leon.jetpack_survivor_maven.worlds;
 
-import com.github.git_leon.jetpack_survivor_maven.actors.sprite.npc.ally.Player;
+import com.github.git_leon.jetpack_survivor_maven.actors.sprite.npc.ally.P1;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,8 +9,9 @@ public class SubSpriteWorldTest {
     @Test
     public void setup() {
         // Given
+        P1 p1 = new P1();
         SubSpriteWorld subSpriteWorld = new SubSpriteWorld();
-        subSpriteWorld.addObject(Player.INSTANCE, 0, 0);
-        Assert.assertTrue(subSpriteWorld.getObjects(Player.class).contains(Player.INSTANCE));
+        subSpriteWorld.addObject(p1, 0, 0);
+        Assert.assertTrue(subSpriteWorld.getObjects(P1.class).contains(p1));
     }
 }
